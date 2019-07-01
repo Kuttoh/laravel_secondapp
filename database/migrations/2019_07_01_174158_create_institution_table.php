e50a52e55f29d6c4fba8f6740f33e1616fe2caa1
Kuttoh <kuttohisaac@gmail.com>
2019-07-01 23:07:35 +0300

    add institution table

--- database/migrations/2019_07_01_174158_create_institution_table.php
+++ database/migrations/2019_07_01_174158_create_institution_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProjectTable extends Migration
+class CreateInstitutionTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateProjectTable extends Migration
      */
     public function up()
     {
-        Schema::create('project', function (Blueprint $table) {
+        Schema::create('institution', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->text('description');
+            $table->integer('phone number');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ class CreateProjectTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('project');
+        Schema::dropIfExists('institution');
     }
 }
